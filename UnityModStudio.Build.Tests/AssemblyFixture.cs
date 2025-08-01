ba7af2be6561308a089826e7ea3ca1fec2e91620
Dark Daskin <darkdaskin@gmail.com>
2025-08-01 12:22:47 +0300

    Use module initializer for Microsoft.Build.Locator.
    This addresses https://github.com/microsoft/testfx/issues/6275

--- UnityModStudio.Build.Tests/AssemblyFixture.cs
+++ UnityModStudio.Build.Tests/AssemblyFixture.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics;
-using System.Globalization;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 using Microsoft.Build.Evaluation;
 using Microsoft.Build.Execution;
 using Microsoft.Build.Locator;
@@ -11,18 +11,16 @@ public static class AssemblyFixture
 {
     public static TestBinaryLogger BinaryLogger { get; } = new();
 
+    [ModuleInitializer]
+    public static void ModuleInitializer() => MSBuildLocator.RegisterDefaults();
+
     [AssemblyInitialize]
     public static void AssemblyInitialize(TestContext context)
     {
-        MSBuildLocator.RegisterDefaults();
-
         CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
 
-        // This must be a separate method, so MSBuildLocator can do its work before MSBuild assemblies are accessed.
-        RegisterGlobalBinaryLogger();
+        ProjectCollection.GlobalProjectCollection.RegisterLogger(BinaryLogger);
     }
-
-    private static void RegisterGlobalBinaryLogger() => ProjectCollection.GlobalProjectCollection.RegisterLogger(BinaryLogger);
 
     // TODO: kill VBCSCompiler
     [AssemblyCleanup]
