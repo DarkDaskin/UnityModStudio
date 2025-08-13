56d436b03863bfd0d817dc35ab7006ec0845039b
Dark Daskin <darkdaskin@gmail.com>
2025-08-13 09:14:48 +0300

    Download Unity Doorstop on build instead of using bundled version.

---
+++ UnityModStudio.Build/Tasks/GetFileVersion.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace UnityModStudio.Build.Tasks;
+
+public class GetFileVersion : Task
+{
+	[Required]
+	public string? Path { get; set; }
+
+	[Output]
+	public string? FileVersion { get; set; }
+
+	public override bool Execute()
+	{
+		if (!File.Exists(Path))
+		{
+			Log.LogError("Specified file does not exist.");
+			return false;
+		}
+
+		FileVersion = FileVersionInfo.GetVersionInfo(Path!).FileVersion;
+		return true;
+	}
+}
